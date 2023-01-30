1abf1671943671edcc2d0d12007f5ff7a66941b4
Alexander Giagoulas <alexander@giagoulas.com>
2023-01-30 09:33:18 +0100

    fix svg styling

--- pages/links.js
+++ pages/links.js
@@ -1,15 +1,6 @@
 import Layout from '../components/layout';
 import TextPage from '../components/text-page';
-
-
-function LinkItem({ url, name, icon }) {
-    return (
-        <a href={url} target="_blank" className="border-2 w-3/4 h-10 md:w-1/2 flex justify-center mx-auto mb-2 rounded-md items-center border-gray-300 text-black hover:bg-gray-800 hover:border-gray-800 hover:text-gray-100">
-            <img src={icon} className="w-4 h-4 mr-1" alt={name} />
-            <p>{name}</p>
-        </a>
-    );
-}
+import SocialLinkItem from '../components/social-link-item';
 
 export default function Links() {
     return (
@@ -18,20 +9,20 @@ export default function Links() {
                 <h3 className="font-semibold text-xl mb-2">Alexander Giagoulas</h3>
                 <div className="mb-4">
                     <h4 className="font-semibold mb-2">Social</h4>
-                    <LinkItem url="https://twitter.com/AGiagoulas" name="Twitter" icon="/icons/twitter.svg" />
-                    <LinkItem url="mailto:alexander@giagoulas.com" name="Mail" icon="/icons/mail.svg" />
+                    <SocialLinkItem url="https://twitter.com/AGiagoulas" name="Twitter" icon="/icons/twitter.svg" />
+                    <SocialLinkItem url="mailto:alexander@giagoulas.com" name="Mail" icon="/icons/mail.svg" />
                 </div>
                 <div className="mb-4">
                     <h4 className="font-semibold mb-2">Photography</h4>
-                    <LinkItem url="https://www.instagram.com/giagoulasphoto" name="Instagram" icon="/icons/instagram.svg" />
-                    <LinkItem url="https://giagoulas.com" name="Website" icon="icons/website.svg" />
-                    <LinkItem url="https://vero.co/giagoulas" name="Vero" icon="/icons/links.svg" />
+                    <SocialLinkItem url="https://www.instagram.com/giagoulasphoto" name="Instagram" icon="/icons/instagram.svg" />
+                    <SocialLinkItem url="https://giagoulas.com" name="Website" icon="icons/website.svg" />
+                    <SocialLinkItem url="https://vero.co/giagoulas" name="Vero" icon="/icons/links.svg" />
                 </div>
                 <div className="mb-4">
                     <h4 className="font-semibold mb-2">Business</h4>
-                    <LinkItem url="https://www.linkedin.com/in/alexander-giagoulas" name="LinkedIn" icon="/icons/linkedin.svg" />
-                    <LinkItem url="https://www.xing.com/profile/AlexanderPhilipp_Giagoulas" name="Xing" icon="/icons/xing.svg" />
-                    <LinkItem url="https://github.com/agiagoulas" name="Github" icon="/icons/github.svg" />
+                    <SocialLinkItem url="https://www.linkedin.com/in/alexander-giagoulas" name="LinkedIn" icon="/icons/linkedin.svg" />
+                    <SocialLinkItem url="https://www.xing.com/profile/AlexanderPhilipp_Giagoulas" name="Xing" icon="/icons/xing.svg" />
+                    <SocialLinkItem url="https://github.com/agiagoulas" name="Github" icon="/icons/github.svg" />
                 </div>
             </TextPage>
         </Layout>
