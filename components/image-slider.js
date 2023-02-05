9abf2b5be2afe29426f44b9cf8f236d9d4e3e481
Alexander Giagoulas <alexander@giagoulas.com>
2023-02-05 19:55:42 +0100

    fix center text position

--- components/image-slider.js
+++ components/image-slider.js
@@ -31,7 +31,7 @@ export default function ImageSlider({ galleries }) {
             {galleries.map(({ _id, title, images }) => (
                 <SwiperSlide key={`${images[0]["key"]}-SwiperSlide`}>
                     <Link href={`/images/${_id}`} key={`${images[0]["key"]}-Link`}>
-                        <div className="relative" key={`${images[0]["key"]}-div1`}>
+                        <div className="relative text-center" key={`${images[0]["key"]}-div1`}>
                             <img src={images[0]["url"]} alt={images[0]["url"]["key"]} key={`${images[0]["key"]}-img`} className="h-[80vh] container object-cover" />
                             <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 sm:left-20 sm:-translate-x-0 text-white items-center" key={`${images[0]["key"]}-div2`}>
                                 <h2 className="font-bold text-2xl sm:text-4xl md:text-6xl xl:text-8xl" key={`${images[0]["key"]}-h2`}>{title.toUpperCase()}</h2>
