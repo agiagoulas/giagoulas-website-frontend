c84a8d2c7cdf5a7e584cb15ffa5a62c1405a10a4
Alexander Giagoulas <alexander@giagoulas.com>
2023-02-05 19:39:00 +0100

    add title to front page

--- components/image-slider.js
+++ components/image-slider.js
@@ -24,14 +24,19 @@ export default function ImageSlider({ galleries }) {
             pagination={{
                 clickable: true,
             }}
-            navigation={false}
+            navigation={true}
             modules={[Autoplay, Pagination, Navigation, Keyboard]}
             className={styles.swiperContainer}
         >
-            {galleries.map(({ _id, images }) => (
-                <SwiperSlide key={images[0]}>
-                    <Link href={`/images/${_id}`}>
-                        <img src={images[0]["url"]} alt={images[0]["url"]} key={images[0]} className="h-[80vh] container object-cover" />
+            {galleries.map(({ _id, title, images }) => (
+                <SwiperSlide key={`${images[0]["key"]}-SwiperSlide`}>
+                    <Link href={`/images/${_id}`} key={`${images[0]["key"]}-Link`}>
+                        <div className="relative" key={`${images[0]["key"]}-div1`}>
+                            <img src={images[0]["url"]} alt={images[0]["url"]["key"]} key={`${images[0]["key"]}-img`} className="h-[80vh] container object-cover" />
+                            <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 sm:left-20 sm:-translate-x-0 text-white items-center" key={`${images[0]["key"]}-div2`}>
+                                <h2 className="font-bold text-2xl sm:text-4xl md:text-6xl xl:text-8xl" key={`${images[0]["key"]}-h2`}>{title.toUpperCase()}</h2>
+                            </div>
+                        </div>
                     </Link>
                 </SwiperSlide>
             ))}
