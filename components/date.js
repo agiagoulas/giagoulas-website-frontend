ad3df75f87ac4864db0f2e0eea9d9b5566048ab2
Alexander Giagoulas <alexander@giagoulas.com>
2023-02-05 16:27:54 +0100

    add time stamp, fix spacing

--- components/date.js
+++ components/date.js
@@ -3,10 +3,14 @@ import { parseISO, format } from 'date-fns';
 export default function Date({ dateString }) {
     const date = parseISO(dateString);
     return (
-        <div className="mb-1 text-sm text-gray-800">
-            <time dateTime={dateString}>
-                {format(date, 'LLLL d, yyyy')}
-            </time>
+        <div className="mb-1 text-sm">
+            {(dateString != null) ?
+                <time className="text-gray-800" dateTime={dateString}>
+                    {format(date, 'dd.MM.yyyy')}
+                </time>
+                :
+                <p className="text-gray-500">-</p>
+            }
         </div>
     );
 }
--- components/gallery-preview.js
+++ components/gallery-preview.js
@@ -1,16 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Date from './date'
 
-export default function GalleryPreview({ id, title, images, summary }) {
+export default function GalleryPreview({ id, title, images, updatedOn }) {
     return (
         <Link href={`/images/${id}`} className="hover:opacity-80">
-            <div className="text-center h-[40vh] shadow-md border border-transparent rounded overflow-hidden">
+            <div className="text-center h-80 shadow-md border border-transparent rounded overflow-hidden">
                 <div className="h-4/5 w-full relative">
                     <Image src={images[0]["url"]} fill alt={images[0]["url"]} sizes="100vh" priority={false} className="object-cover object-center" />
                 </div>
                 <div className="flex justify-center items-center flex-col h-1/5">
                     <h2 className="font-semibold">{title}</h2>
-                    <p>{summary}</p>
+                    <Date dateString={updatedOn} />
                 </div>
             </div>
         </Link>
